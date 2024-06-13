c0ca2cf96360f51af67926fba8b44a3810b604ad
Beatriz Cavalcante <121120555+Beatriiz-Cavalcante@users.noreply.github.com>
2024-06-13 14:59:25 -0300

    componente footer

---
+++ components/footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const Footer = ({ onMenuPress }) => {
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity>
+      <Image
+        source={require("../assets/footer/homeSelec.png")}
+        style={styles.image}
+      />
+      </TouchableOpacity>
+      
+      <TouchableOpacity>
+      <Image
+        source={require("../assets/footer/add.png")}
+        style={styles.imageAdd}
+      />
+      </TouchableOpacity>
+      
+
+      <TouchableOpacity onPress={onMenuPress}>
+      <Image
+        source={require("../assets/footer/receitas.png")}
+        style={styles.image}
+      />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    width: "100%",
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    backgroundColor: "#F28705",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    marginTop: 70,
+  },
+  imageAdd: {
+    marginBottom: 20,
+  }
+});
+
+export default Footer;
